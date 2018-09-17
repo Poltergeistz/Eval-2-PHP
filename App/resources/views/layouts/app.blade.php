cae77595308e7c0b4045f2919ca09523868675e3
david <david.journo.31@gmail.com>
2018-09-17 15:35:46 +0200

    Dashboard route

--- App/resources/views/layouts/app.blade.php
+++ App/resources/views/layouts/app.blade.php
@@ -60,8 +60,8 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                     <a class="dropdown-item" href="/post/create">
-                                        {{ __('Creer une annonce') }}
+                                     <a class="dropdown-item" href="/home">
+                                        {{ __('Dashboard') }}
                                     </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
