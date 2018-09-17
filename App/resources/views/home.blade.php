6301de6b54eb3363420bad6cc27d5ec265245b63
david <david.journo.31@gmail.com>
2018-09-17 15:36:17 +0200

    Dashboard view modifications

--- App/resources/views/home.blade.php
+++ App/resources/views/home.blade.php
@@ -14,7 +14,21 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Vous êtes connecté! 
+                    <p>Bonjour {{ Auth::user()->name }} !</p>
+                    <h5>Donnez des objets qui ne vous servent plus pour gagner de l'XP</h5>
+                    <small>Un objet donné vaut 25xp</small>
+                    <!-- Lvl bar -->
+                    <h5>Niveau : </h5>
+                    <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+                    </div>
+                    <!-- lvl bar end  -->
+                    <!-- If the xp is 100 show the buttons for leveling up or exchange for virtual money -->
+                    <div class="dropdown-divider"></div>
+                    <a href="/" class="btn btn-warning" role="button">Level UP !</a>
+                    <a href="/" class="btn btn-success" role="button">BRIQUES !</a>
+                    
                 </div>
             </div>
         </div>
